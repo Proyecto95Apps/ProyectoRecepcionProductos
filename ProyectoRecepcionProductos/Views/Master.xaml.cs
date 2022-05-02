1ec6c1df9bb2ca871b7b1b3803bb62f42495b200
12345DavidAvila <94076545+12345DavidAvila@users.noreply.github.com>
2022-05-01 20:20:34 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoRecepcionProductos/Views/Master.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProyectoRecepcionProductos
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Master : ContentPage
+    {
+        public Master()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAcerca_Clicked(object sender, EventArgs e)
+        {
+            App.MasterDet.IsPresented = false;
+            await App.MasterDet.Detail.Navigation.PushAsync(new Acercaxaml());
+        }
+
+        private async void btnVentas_Clicked(object sender, EventArgs e)
+        {
+            App.MasterDet.IsPresented = false;
+            await App.MasterDet.Detail.Navigation.PushAsync(new Ventas());
+        }
+    }
+}
